4f4f587a6d931801d00bf8b5c19e54498b4d19c6
K-Tanish <tanishhk99@gmail.com>
2026-04-03 10:34:28 +0530

    Adding Features, Learn, and Detect Disease buttons to navbar

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -1,39 +1,48 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-
 function Navbar() {
     return (
-
-        <nav class="navbar navbar-expand-lg bg-dark border-bottom fixed-top">
-            <div class="container p-2">
-                <a class="navbar-brand logo" href="/">
+        <nav className="navbar navbar-expand-lg bg-dark border-bottom fixed-top">
+            <div className="container p-2">
+                <a className="navbar-brand logo" href="/">
                     <img src='./krishilogobg.png' className='ps-4' alt="Logo"
-                        style={{ width: "70px", height: "80px", marginRight: "8px" }}></img>
-
+                        style={{ width: "70px", height: "80px", marginRight: "8px" }} />
                     <Link className='home ps-4' to="/">Home</Link>
-
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
+
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 nav-links">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 nav-links align-items-center gap-2">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/signup">Sign up</Link>
+                            <Link className="nav-link" to="/features">Features</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/login">Log in</Link>
+                            <Link className="nav-link" to="/learn">Learn</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/ecommerce">E-commerce</Link>
+                            <Link className="nav-link detect-btn" to="/detect-disease">🔬 Detect Disease</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link signup-btn" to="/signup">Sign up</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link login-btn" to="/login">Log in</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link ecommerce-btn" to="/ecommerce">E-commerce</Link>
                         </li>
                     </ul>
                 </div>
-
             </div>
         </nav>
-
     );
 }
 
